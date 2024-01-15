3df7ca963ff87307528c5bb161b4851b52540709
kariuki-ian <kariukii@student.douglascollege.ca>
2024-01-14 19:27:51 -0800

    Initial Commit

---
+++ src/test/java/com/csis3275/assignment1ika09/Assignment1Ika09ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csis3275.assignment1ika09;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Assignment1Ika09ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
